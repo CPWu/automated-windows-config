34112a0988b8ccaf7a98fe438f63ae01efe4ce96
Chun Wu <the.chun.wu@gmail.com>
2021-12-14 17:02:02 +0000

    feat(update): Updated Software List

--- configure.ps1
+++ configure.ps1
@@ -1,10 +1,13 @@
 # Installs Chocolately onto your machine
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-choco install git -y
+choco install git.install -y
 choco install vscode -y 
 choco install docker-desktop -y 
 choco install kubernetes-cli -y
 choco install kubernetes-helm -y
 choco install 7zip.install -y
-chocl install vagrant -y
+choco install lens -y
+choco install azure-cli -y
+choco install googlechrome -y
+choco install velero -y
